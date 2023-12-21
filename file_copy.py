1adf9c363f72c6a0b4dc651c99e0180813fd4b5a
Vityuk Yuriy <you.thunderstruck@gmail.com>
2023-12-21 18:20:27 +0200

    add cli comand

--- file_copy.py
+++ file_copy.py
@@ -26,37 +26,30 @@
    -- виклик перевірки чибув переданий кінцева точка. якшо ні, створити папку в "звідки"
      
 """
+# оброка винятків? // // якшо доступ заборонений до файлу??
 
-# ця штука має приймати аргументи з терміналу?
-# не розумію де має бути тека нова "діст" якшо вона не була вказана, треба її робити в тій самі теці звідки відбувалось копіювання? чи в місці виклику?
-# оброка винятків?
-# p.exists() - рмк чекери наявності
 from pathlib import Path
 import shutil
-
-base_folder = Path()
+import os
+import argparse
 
 
 def create_directory(path, directory_name):
-    # Ensure path is a pathlib.Path object
-    path = Path(path)
-    # Create the new directory inside the specified path
     new_directory_path = path / directory_name
-    try:
-        # Create the directory
-        new_directory_path.mkdir()
-        return new_directory_path
-    except FileExistsError:
+    if new_directory_path.exists() and new_directory_path.is_dir():
         print(f"Directory '{new_directory_path}' already exists at {directory_name}")
         return new_directory_path
-    except Exception as e:
-        print(f"Error creating directory: {e}")
+    else:
+        try:
+            new_directory_path.mkdir(parents=True, exist_ok=True)
+            return new_directory_path
+        except Exception as e:
+            print(f"Error creating directory: {e}")
 
 
-def folder_cheker(file_ext, dist_path):
-    folder_list = dist_path.iterdir()
-    folder_list_name = [data.name for data in folder_list]
-    if file_ext in folder_list_name:
+def get_dir_reciever(file_ext, dist_path):
+    dist_folder = dist_path / file_ext
+    if dist_folder.exists():
         path = dist_path / file_ext
         return path
     else:
@@ -66,22 +59,19 @@ def folder_cheker(file_ext, dist_path):
 
 
 def file_copy_handler(file, dest_path):
-    file_suf = file.suffix
-    destination_folder = folder_cheker(file_suf, dest_path)
-    dest_file_list = destination_folder.iterdir()
-    list_file_name = [data.name for data in dest_file_list]
+    destination_folder = get_dir_reciever(file.suffix, dest_path)
+    destination_file = destination_folder / file.name
 
-    if file.name in list_file_name:
+    if destination_file.exists():
         print(f"In directory '{destination_folder}' already exists file {file.name}")
     else:
-        source_path = Path(file)
+        source_path = file
         destination_path = Path(destination_folder)
-        # Copy the file using shutil.copy
         try:
             shutil.copy(source_path, destination_path)
+            print(f"File {file.name} is succesfuly copied at {destination_path}")
         except Exception as ex:
             print(f"File {file.name} cannot be copied. Reason: {ex}")
-        print(f"File {file.name} is succesfuly copied at {destination_path}")
 
 
 def folder_handler(path, new_directory_path):
@@ -93,14 +83,35 @@ def folder_handler(path, new_directory_path):
             folder_handler(data, new_directory_path)
 
 
-def copy_handler(origin_path, destination_folder="dist2"):
-    if type(destination_folder) == str:
+def main():
+    parser = argparse.ArgumentParser(description="Process command-line arguments")
+
+    parser.add_argument("--source", "-s", required=True)
+    parser.add_argument("--dist", "-d", default="dist")
+
+    args = parser.parse_args()
+
+    source_str = os.path.abspath(args.source)
+    source_path = Path(source_str)
+
+    if not source_path.exists():
+        print("Sorry, source folder not found")
+        return
+
+    if args.dist == "dist":
         p = Path()
-        destination_folder = create_directory(p, destination_folder)
-    folder_handler(origin_path, destination_folder)
+        dist_path = create_directory(p, args.dist)
+
+    else:
+        dist_str = os.path.abspath(args.dist)
+        dist_path = Path(dist_str)
+
+    if not dist_path.exists():
+        print("Sorry, destination folder not found")
+        return
+
+    folder_handler(source_path, dist_path)
 
 
-test_path = base_folder / "test_dir"
-dest_path = create_directory(base_folder, "manual_dist")
-
-copy_handler(test_path)
+if __name__ == "__main__":
+    main()
